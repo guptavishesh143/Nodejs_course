bdd059bcc540e5f65696dd4b35ff67dd1b36aa2e
vishesh.gupta <vishesh.gupta@aceturtle.com>
2022-12-14 15:31:33 +0530

    removed un-used code

--- notes-app/NoteApp/app.js
+++ notes-app/NoteApp/app.js
@@ -1,27 +1,10 @@
-// import yargs from "yargs";
-
-// // const yargs = require('yargs')
-
-// console.log(process.argv);
-// console.log(`yargs console`,yargs.argv);
-
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
-console.log(yargs(hideBin(process.argv)).argv);
+//console.log(yargs(hideBin(process.argv)).argv);
 // yargs.version('1.1.0')
 
-//create add command
-
-// yargs.command({
-//     command:'add',
-//     describe:'Add a new note',
-//     handler: function (){
-//         console.log('Adding a new note');
-//     }
-// })
-
-//Add Command 
+//Add Command
 
 yargs(hideBin(process.argv))
   .command({
@@ -33,33 +16,36 @@ yargs(hideBin(process.argv))
   })
   .parse();
 
-//New Command Remove  
+//New Command Remove
 
-yargs(hideBin(process.argv)).command({
-  command: "add",
-  describe: "Remove a note",
-  handler: function () {
-    console.log("Remove notes");
-  },
-}).parse()
+yargs(hideBin(process.argv))
+  .command({
+    command: "add",
+    describe: "Remove a note",
+    handler: function () {
+      console.log("Remove notes");
+    },
+  })
+  .parse();
 
-
-//list command 
-yargs(hideBin(process.argv)).command({
-    command:'list',
-    describe:'Print the placeholder text message',
-    handler:function(){
+//list command
+yargs(hideBin(process.argv))
+  .command({
+    command: "list",
+    describe: "Print the placeholder text message",
+    handler: function () {
       console.log(`My name is vishesh gupta`);
-    }
-}).parse()
+    },
+  })
+  .parse();
 
-
-//read command 
-yargs(hideBin(process.argv)).command({
-    command:'read',
-    describe:'Print the placeholder message ',
-    handler:function(){
-        console.log("You can read the message from here.");
-    }
-}).parse()
-
+//read command
+yargs(hideBin(process.argv))
+  .command({
+    command: "read",
+    describe: "Print the placeholder message ",
+    handler: function () {
+      console.log("You can read the message from here.");
+    },
+  })
+  .parse();
