bfbd6bb7a7c0ce1f6812282aa82dbfad33032edc
vishesh.gupta <vishesh.gupta@aceturtle.com>
2022-12-14 15:30:25 +0530

    yargs build commnad

--- notes-app/NoteApp/app.js
+++ notes-app/NoteApp/app.js
@@ -1,11 +1,65 @@
-import validator from "validator";
+// import yargs from "yargs";
 
-//validator for in-built validation function
+// // const yargs = require('yargs')
 
-//check email ===>
-const emailCheck =  validator.isEmail('guptavihsesh2332@gmail.adin');
+// console.log(process.argv);
+// console.log(`yargs console`,yargs.argv);
+
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+console.log(yargs(hideBin(process.argv)).argv);
+// yargs.version('1.1.0')
+
+//create add command
+
+// yargs.command({
+//     command:'add',
+//     describe:'Add a new note',
+//     handler: function (){
+//         console.log('Adding a new note');
+//     }
+// })
+
+//Add Command 
+
+yargs(hideBin(process.argv))
+  .command({
+    command: "add",
+    describe: "Adding command",
+    handler: function () {
+      console.log("Adding notes");
+    },
+  })
+  .parse();
+
+//New Command Remove  
+
+yargs(hideBin(process.argv)).command({
+  command: "add",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Remove notes");
+  },
+}).parse()
 
 
-//chalk ( for theming)
+//list command 
+yargs(hideBin(process.argv)).command({
+    command:'list',
+    describe:'Print the placeholder text message',
+    handler:function(){
+      console.log(`My name is vishesh gupta`);
+    }
+}).parse()
 
+
+//read command 
+yargs(hideBin(process.argv)).command({
+    command:'read',
+    describe:'Print the placeholder message ',
+    handler:function(){
+        console.log("You can read the message from here.");
+    }
+}).parse()
 
---
+++ notes-app/NoteApp/yargs.js
@@ -0,0 +1,5 @@
+import yargs from "yargs";
+// yargs.version('1.1.0')
+
+console.log(process.argv);
+console.log(yargs.argv);
